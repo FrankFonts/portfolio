73e70ffe1b8e652eae95d696f6c62389194286ae
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2023-07-17 14:54:27 +0200

    standalone components test and routing test

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,29 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AboutComponent } from './components/about/about.component';
+import { Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AboutComponent],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
     </a>
+    <app-about></app-about>
   `,
 })
 export class App {
   name = 'Angular';
+
+  routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'second-component', component: ContactComponent },
+  ];
 }
 
 bootstrapApplication(App);
